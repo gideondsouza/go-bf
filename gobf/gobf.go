14824c0dfbe20202e5f89811a86c843abcdaafcf
Gideon Dsouza <gideon.dsouza@algorisys.com>
2017-07-09 23:35:55 +0530

    fleshing out some possible structure.

--- gobf/gobf.go
+++ gobf/gobf.go
@@ -1,1 +1,26 @@
 package gobf
+
+type Program struct {
+	mem []byte
+	pos int
+}
+
+func (p *Program) getBytAt(i int) byte {
+	return 0
+}
+
+func (p *Program) incAt(i int) {
+
+}
+
+func (p *Program) decAt(i int) {
+
+}
+
+func (p *Program) out(i int) {
+
+}
+
+func (p *Program) in(b byte) {
+
+}
